b0e90890b065c7ded78eaf5a27f8372d0bc31bac
Aarfdark <aadarsh.natarajan@gmail.com>
2025-12-24 21:54:49 -0500

    HAHAHAHAHAH YOU CAN CLICK TO BUILDING

--- scripts/board_controller.gd
+++ scripts/board_controller.gd
@@ -2,20 +2,24 @@ extends TileMapLayer
 
 const main_atlas_id = 0
 enum {NONE=-1, LILYPAD=0, FLY=1}
-var cur_building: int
+var cur_building: int = NONE
+var building_dict = {
+	NONE : Vector2i(2, 0), # water
+	LILYPAD : Vector2i(0,0),
+	FLY : Vector2i(1,0),
+}
 
 func set_building(type):
 	cur_building = type
-	print(type)
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 			var global_clicked = event.position
 			var pos_clicked = local_to_map(to_local(global_clicked))
-			var cur_atlas_coords = get_cell_atlas_coords(pos_clicked)
+			#var cur_atlas_coords = get_cell_atlas_coords(pos_clicked)
 			var cur_tile_alt = get_cell_alternative_tile(pos_clicked)
 			# there is a tile where the mouse was clicked
 			if cur_tile_alt != -1:
-				var total_num_alts = tile_set.get_source(main_atlas_id).get_alternative_tiles_count(cur_atlas_coords)
-				set_cell(pos_clicked, main_atlas_id, cur_atlas_coords, (cur_tile_alt+1)%total_num_alts )
+				#var total_num_alts = tile_set.get_source(main_atlas_id).get_alternative_tiles_count(cur_atlas_coords)
+				set_cell( pos_clicked, main_atlas_id, building_dict[cur_building] )
