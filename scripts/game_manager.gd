2cf7a8bcdc9795b8148ea194677406f31fbf5d64
Aarfdark <aadarsh.natarajan@gmail.com>
2026-02-01 19:59:58 -0500

    fixed some art stuff

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,10 +2,10 @@
 extends Node2D
 
 #@onready var building_container: HBoxContainer = $Control/BuildingUI/MarginContainer/BuildingContainer
-@onready var building_container: VBoxContainer = $Control/BuildingUI2/MarginContainer2/BuildingContainer2
+@onready var building_container: VBoxContainer = $Control/BuildingUI/MarginContainer/BuildingContainer
 @onready var board_controller: TileMapLayer = $Board
 
-enum {NONE=-1, LILYPAD=0, FLY=1}
+enum {NONE=-1, LILYPAD=0, FLY=1, FLOWER=2}
 var building_buttons: Array
 
 func _ready():
@@ -15,9 +15,12 @@ func _ready():
 	
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_MIDDLE and event.is_pressed():
 			for button in building_buttons:
 				button.button_pressed = false
+				
+	if event.is_action_pressed("quit"):
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func building_select(toggled, cur_button):
 	# if a button is selected, deselect all the others
@@ -26,8 +29,9 @@ func building_select(toggled, cur_button):
 			if button != cur_button:
 				button.button_pressed = false
 		# send the selected building type to the board_controller
-		match cur_button.text:
-			"Lilypad": board_controller.set_building(LILYPAD)
-			"Fly": board_controller.set_building(FLY)
+		match cur_button.get_meta("id"):
+			"lilypad": board_controller.set_building(LILYPAD)
+			"fly": board_controller.set_building(FLY)
+			"flower": board_controller.set_building(FLOWER)
 	if !toggled:
 		board_controller.set_building(NONE)
